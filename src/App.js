b32e90f4965992540e02427c4c01eef109e73616
lorenzo amodeo <lrnzmd@gmail>
2020-09-14 11:59:22 +0200

    inizio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    <div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
     </div>
   );
 }
